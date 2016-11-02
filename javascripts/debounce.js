65fa5fba23e4c85943eec035a07fb69439d7a7b0
vflyson <vflyson@users.noreply.github.com>
2016-11-02 00:23:35 -0400

    rewrite in VueJS, restyle the interface, implement routing

---
+++ javascripts/debounce.js
@@ -0,0 +1,17 @@
+/* Ripped out of underscorejs (http://underscorejs.org) which is licensed under MIT */
+
+function debounce (func, wait, immediate) {
+    var timeout;
+
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    }
+}
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,120 +1,307 @@
+'use strict'
+
+
+var baseURL = 'https://api.github.com/repos/Lyrics/lyrics/contents/database/'
 var debounceTime = 1000;
 
-var $content = $('#content');
 
-function doGithubsJob (input) {
-    return input.replace(/\?/g, '%3F').replace('%3Fref=', '?ref=');
-}
+/* API */
 
-function list (path) {
-    $.get('https://api.github.com/repos/Lyrics/lyrics/contents/' + path)
-        .done(function( data ) {
-            if (data.length) {
-                $content.empty();
-                data.forEach(function (item) {
-                    var url = doGithubsJob(item.html_url);
-
-                    var $item = $('<div><a href="' + url + '">' + item.name  + '</a></div>');
-
-                    $item.click(function (event) {
-                        event.preventDefault();
-
-                        if (item.path.split('/').length < 5)
-                            list(item.path);
-                        else
-                            retrieve(item.download_url);
-                    });
-
-                    $content.append($item);
-                });
+function APIreadPath (path, cb) {
+    Vue.http.get(baseURL + path).then(
+        function(response) {
+            if (response.ok) {
+                cb(null, response.body)
             } else {
-                $content.text("Empty directory");
+                cb(new Error('Empty directory'))
             }
+        },
+        function(response) {
+            const reason = response.status == 403 ? 'API rate limit exceeded, please wait a bit before trying again' : 'Something went wrong'
+            cb(new Error(reason))
         })
-        .fail(function() {
-            $content.text("");
-        });
-};
-
-function retrieve (file) {
-    $.get(file)
-        .done(function( data ) {
-            $content.text(data);
-        })
-        .fail(function() {
-            $content.text("");
-        });
 }
 
-function retrieveIndirect (file) {
-    $.get(file)
-        .done(function(data) {
-            $.get(data.download_url)
-                .done(function(data) {
-                    $content.text(data);
+function APIreadFile (path, cb) {
+    Vue.http.get(baseURL + path).then(
+        function(response) {
+            Vue.http.get(response.body.download_url).then(
+                function (response) {
+                    cb(null, response.body)
+                },
+                function (response) {
+                    cb(new Error("Something went wrong again"))
                 })
-                .fail(function() {
-                    $content.text("Something went wrong again");
-                });
+        },
+        function(response) {
+            const reason = response.status == 403 ? 'API rate limit exceeded, please wait a bit before trying again' : 'Something went wrong'
+            cb(new Error(reason))
         })
-        .fail(function() {
-            $content.text("Something went wrong");
-        });
 }
 
-function find () {
-    var query = $(this).val().trim();
-
+function APIsearch (query, cb) {
     if (query.length < 3) {
-        $content.text("");
-        return;
+        cb(new Error("Query too short"))
+    } else {
+        Vue.http.get('https://api.github.com/search/code?q=repo:Lyrics/lyrics path:database/ fork:false ' + query)
+            .then(function (response) {
+                cb(null, response.body.items)
+            },
+            function (response) {
+                const reason = response.status == 403 ? 'Search API rate limit exceeded, please try searching again later' : 'Something went wrong'
+                cb(new Error(reason))
+            })
+    }
+}
+
+
+/* Breadcrumbs */
+
+var breadcrumbsData = []
+
+function updateBreadcrumbs(route) {
+    if (breadcrumbsData.length) {
+        for (var i = breadcrumbsData.length; i >= 0; i--)
+            Vue.delete(breadcrumbsData, i)
     }
 
-    $.get('https://api.github.com/search/code?q=repo:Lyrics/lyrics path:database/ fork:false ' + query)
-        .done(function( data ) {
-            if (data.total_count) {
-                $content.empty();
+    if (!route) return;
 
-                data.items.forEach(function (item) {
-                    var path = item.path.split('/'); path.shift(); path.shift();
-                    var url = doGithubsJob(item.html_url);
+    var usedParams = [];
+    var i = 1;
+    for (var pname in route.params) {
+        if (!route.params[pname]) continue;
 
-                    var $div = $('<div></div>');
-                    var $composition = $('<a href="' + url + '">' + item.name  + '</a>');
-                    var $artist = $('<a href="' + url + '/../..">' + path[0] + '</a>');
-                    var $album = $('<a href="' + url + '/..">' + path[1] + '</a>');
-                    $div.append($composition, ' by ', $artist, ' from album ', $album);
+        var path = ('/db/' + usedParams.join('/') + '/' + route.params[pname]).replace(/\/+/g, '/')
+        Vue.set(breadcrumbsData, i++, { path: path, name: route.params[pname] })
+        usedParams.push(route.params[pname])
+    }
+}
 
-                    $content.append($div);
 
-                    $composition.click(function (event) {
-                        event.preventDefault();
-                        retrieveIndirect(doGithubsJob(item.url));
-                    });
-                    $artist.click(function (event) {
-                        event.preventDefault();
-                        list(doGithubsJob(item.path) + '/../..');
-                    });
-                    $album.click(function (event) {
-                        event.preventDefault();
-                        list(doGithubsJob(item.path) + '/..');
-                    });
-                });
-            } else {
-                $content.text("Nothing found");
+/* / */
+
+const Home = {
+    created: function () {
+        document.title = 'Lyrics'
+        updateBreadcrumbs()
+    },
+    template: '<div>Please start by picking a letter or using the search field</div>'
+}
+
+
+/* /db/:letter/:artist/:album */
+
+const Path = {
+    data () {
+        return {
+            loading: false,
+            items: null,
+            error: null
+        }
+    },
+    created () {
+        this.fetchData()
+    },
+    watch: {
+        '$route': 'fetchData'
+    },
+    methods: {
+        fetchData () {
+            this.error = this.items = null
+            this.loading = true
+
+            APIreadPath([
+                this.$route.params.letter,
+                this.$route.params.artist,
+                this.$route.params.album
+            ].join('/').replace(/\/*$/, ''), (err, items) => {
+                this.loading = false
+
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.items = items
+                }
+
+                var title = []
+
+                if (this.$route.params.album)
+                    title.push(this.$route.params.album)
+                if (this.$route.params.artist)
+                    title.push(this.$route.params.artist)
+                if (this.$route.params.letter)
+                    title.push(this.$route.params.letter)
+
+                title.push('Lyrics')
+
+                document.title = title.join(' | ')
+
+                updateBreadcrumbs(this.$route)
+            })
+        }
+    },
+    template: `<div class="path">
+                <div class="loading" v-if="loading">Loading…</div>
+                <div v-if="error" class="error">{{ error }}</div>
+                <transition name="slide">
+                 <div v-if="items" class="content">
+                  <ul id="ls">
+                   <li v-for="item in items"><router-link :to="{ path: item.name }" append>{{ item.name }}</a></li>
+                  </ul>
+                 </div>
+                </transition>
+                </div>`
+}
+
+
+/* /db/:letter/:artist/:album/:song */
+
+const File = {
+    data () {
+        return {
+            loading: false,
+            text: null,
+            error: null
+        }
+    },
+    created () {
+        this.fetchData()
+    },
+    watch: {
+        '$route': 'fetchData'
+    },
+    methods: {
+        fetchData () {
+            this.error = this.text = null
+            this.loading = true
+
+            APIreadFile([
+                this.$route.params.letter,
+                this.$route.params.artist,
+                this.$route.params.album,
+                this.$route.params.song
+            ].join('/').replace(/\/*$/, ''), (err, text) => {
+                this.loading = false
+
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.text = text
+                }
+
+                document.title = this.$route.params.song + ' – ' + this.$route.params.artist + ' |  Lyrics'
+
+                updateBreadcrumbs(this.$route)
+            })
+        }
+    },
+    template: `<div class="file">
+                 <div class="loading" v-if="loading">Loading…</div>
+                 <div v-if="error" class="error">{{ error }}</div>
+                 <transition name="slide">
+                  <div v-if="text" class="content"><pre>{{ text }}</pre></div>
+                 </transition>
+               </div>`
+}
+
+
+Vue.component('search-query', {
+    data () {
+        return {
+            query: '',
+        }
+    },
+    methods: {
+        find () {
+            if (this.query.length) {
+                this.$router.push('/search/' + this.query)
             }
-        })
-        .fail(function() {
-            $content.text("Rate limiting is in place, please hold on for a bit");
-        });
-};
+        }
+    },
+    template: '<input id="query" type="search" v-on:input="find" v-model="query" placeholder="Search" />'
+})
 
-$('#main-content a').click(function (event) {
-        event.preventDefault();
+const Search = {
+    data () {
+        return {
+            loading: false,
+            items: null,
+            error: null
+        }
+    },
+    created () {
+        document.title = 'Search | Lyrics'
+        updateBreadcrumbs()
+        this.fetchData()
+    },
+    watch: {
+        '$route': 'fetchData'
+    },
+    methods: {
+        fetchData: debounce(function () {
+            this.error = this.items = null
+            this.loading = true
 
-        var letter = $(this).text();
+            APIsearch(this.$route.params.query, (err, items) => {
+                this.loading = false
 
-        list('database/' + letter);
-    });
+                if (err) {
+                    this.error = err.toString()
+                } else {
+                    this.items = items
+                }
+            })
+        }, debounceTime)
+    },
+    template: `<div class="search">
+                <div class="loading" v-if="loading">Loading…</div>
+                <div v-if="error" class="error">{{ error }}</div>
+                <transition name="slide">
+                 <div v-if="items" class="content">
+                  <ul id="ls">
+                   <li v-for="item in items"><router-link :to="{ path: '/db' + item.path.substring(8) }">{{ item.name }}</a></li>
+                  </ul>
+                 </div>
+                </transition>
+                </div>`
+}
 
-$('#search').on('input', _.debounce( find, debounceTime ) );
+
+/* Set up the routing */
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    linkActiveClass: 'active',
+    routes: [
+        {
+            path: '/',
+            component: Home
+        },
+        {
+            path: '/db/:letter/:artist?/:album?',
+            component: Path
+        },
+        {
+            path: '/db/:letter/:artist/:album/:song',
+            component: File
+        },
+        {
+            path: '/search/:query',
+            component: Search
+        }
+    ]
+})
+
+
+/* Initiate the framework */
+
+new Vue({router,
+    data: function () {
+        return {
+            alphabet: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
+            breadcrumbs: breadcrumbsData
+        }
+    }
+}).$mount('#app')
