0bbd8daa289cdacafac9e77e95dd2d05f71bad01
Lyrics Admin <open.lyrics.database@gmail.com>
2020-12-18 02:28:03 +0000

    Update website content

--- js/search.js
+++ js/search.js
@@ -1,105 +1,109 @@
-var baseURL = 'https://api.github.com/repos/Lyrics/lyrics/contents/database/'
-var searchBaseURL = 'https://api.github.com/search/code?q=repo:Lyrics/lyrics path:database/ fork:false '
-var debounceTime = 1000
-var dbPrefix = '/db'
+var searchBaseUrl = 'https://api.github.com/search/code?q=repo:Lyrics/lyrics path:database/ '
+var dBWebsitePrefix = '/db'
 
 function formatURL (input) {
     input = encodeURI(input).replace(/\?/g, '%3F')
-    return dbPrefix + input + '/'
+    return dBWebsitePrefix + input + '/'
 }
 
+function getQueryVariable(variableName) {
+    const params = new URLSearchParams(window.location.search);
+    return params.get(variableName);
+}
 
-/* API */
+function reset() {
+    appNode.innerHTML = '';
+}
 
-var API = {
-    search: function (query, cb) {
-        if (query.length < 3) {
-            cb(new Error("Query too short"))
-        } else {
-            Vue.http.get(searchBaseURL + query).then(
-                function (response) {
-                    cb(null, response.body.items)
-                },
-                function (response) {
-                    var reason = response.status == 403 ?
-                        'Search API rate limit exceeded, please try searching again later' :
-                        'Something went wrong'
-                    cb(new Error(reason))
-                })
+function displayLoading() {
+    reset();
+
+    var loadingNode = document.createElement('div');
+    loadingNode.className = 'loading';
+    loadingNode.innerText = 'Loading…';
+
+    appNode.appendChild(loadingNode);
+}
+
+function displayError(reason) {
+    reset();
+
+    var errorNode = document.createElement('div');
+    errorNode.className = 'error';
+    errorNode.innerText = reason;
+
+    appNode.appendChild(errorNode);
+}
+
+function displayResults(items) {
+    reset();
+
+    var contentNode = document.createElement('div');
+    contentNode.className = 'content';
+
+    if (items.length > 0) {
+        var listNode = document.createElement('ul');
+        listNode.id = 'l';
+
+        for (var i = 0; i < items.length; i++) {
+            var item = items[i];
+
+            var listItemNode = document.createElement('li');
+            var listItemAnchorNode = document.createElement('a');
+
+            listItemAnchorNode.className = 't3';
+            listItemAnchorNode.innerText = item.path.split("/")[2] + ' – ' + item.name;
+            listItemAnchorNode.href = formatURL(item.path.substring(8));
+
+            listItemNode.appendChild(listItemAnchorNode);
+            listNode.appendChild(listItemNode);
         }
+
+        contentNode.appendChild(listNode);
+    } else {
+        var noResultsNode = document.createElement('p');
+        noResultsNode.innerText = 'No results';
+        contentNode.appendChild(noResultsNode);
     }
+
+    appNode.appendChild(contentNode);
 }
 
-var Search = {
-    data: function () {
-        return {
-            init: true,
-            loading: false,
-            error: null,
-            items: null
+var queryString = getQueryVariable('q');
+
+// Need to wait for the search field to be appended to the DOM tree
+setTimeout(function () {
+    document.getElementsByTagName('input')[0].value = queryString;
+});
+
+var appNode = document.getElementById('app');
+
+// Put the loading indicator up
+
+displayLoading();
+
+// Proceed to retrieving results from the server
+
+if (queryString.length > 2) {
+    // Make a HTTP Request
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = processHttpResponse;
+    xhr.open("GET", searchBaseUrl + queryString);
+    xhr.send();
+
+    function processHttpResponse() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status == 200) {
+                displayResults(JSON.parse(xhr.responseText).items)
+            } else {
+                displayError(
+                    xhr.status == 403 ?
+                    'Search API rate limit exceeded, please try again in a bit' :
+                    'Something went wrong'
+                );
+            }
         }
-    },
-    created: function () {
-        setTimeout(function () {
-            document.getElementsByTagName('input')[0].value = this.$route.query.q
-        }.bind(this))
-        this.fetchData()
-    },
-    mounted: function () {
-        this.$el.className = 'search'
-    },
-    watch: {
-        '$route': 'fetchData'
-    },
-    methods: {
-        fetchData: debounce(function () {
-            this.init = false
-            this.loading = true
-            this.error = this.items = null
-
-            API.search(this.$route.query.q, function (err, items) {
-                this.loading = false
-
-                if (err) {
-                    this.error = err.toString()
-                } else if (items.length) {
-                    this.items = items
-                }
-            }.bind(this))
-        }, debounceTime)
-    },
-    template: '<div>' +
-                '<div class="loading" v-if="loading">Loading…</div>' +
-                '<div v-if="error" class="error">{{ error }}</div>' +
-                '<div v-if="items" class="content">' +
-                  '<ul id="l">' +
-                    '<li v-for="item in items"><a class="t3" :href="formatURL(item.path.substring(8))">{{ item.path.split("/")[2] }} – {{ item.name }}</a></li>' +
-                  '</ul>' +
-                '</div>' +
-                '<div v-if="!init && !loading && !error && !items" class="content">' +
-                  '<p>No results</p>' +
-                '</div>' +
-              '</div>'
+    }    
+} else {
+    displayError('Query too short');
 }
-
-
-/* Set up the routing */
-
-Vue.use(VueRouter)
-
-var router = new VueRouter({
-    mode: 'history',
-    routes: [
-        {
-            path: '/search.html',
-            component: Search
-        }
-    ]
-})
-
-/* Initiate the framework */
-
-new Vue({
-    router: router,
-    data: { }
-}).$mount('#app')
